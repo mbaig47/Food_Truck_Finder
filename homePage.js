840e5a18d3f9d4c4759ce2d1e843709be4267d9f
Mohammad Abdul Salam <mabdul26@uic.edu>
2019-04-02 18:00:29 -0500

    Icons and space for nearby

--- homePage.js
+++ homePage.js
@@ -1,1 +1,8 @@
 // Initialize and add the map
+
+function currentLocation(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let latlongvalue = position.coords.latitude + "," + position.coords.longitude;
+    l
+}
