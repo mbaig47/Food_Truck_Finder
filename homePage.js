02635599228e281154d2b6ccd7070394697a1c29
Mohammad Abdul Salam <mabdul26@uic.edu>
2019-04-02 19:38:06 -0500

    added 4 buttons to the homepage

--- homePage.js
+++ homePage.js
@@ -1,8 +1,2 @@
 // Initialize and add the map
 
-function currentLocation(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    let latlongvalue = position.coords.latitude + "," + position.coords.longitude;
-    l
-}
